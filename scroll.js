d4deffc74824fbaef11effd711ddbcc6d720d304
DaruneChen <darunechen@gmail.com>
2025-11-29 03:43:46 -0500

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,46 @@
+$(window).on('load', function () {
+    $(".btn-nav").on("click tap", function () {
+        $(".nav-content").toggleClass("showNav hideNav").removeClass("hidden");
+        $(this).toggleClass("animated");
+    });
+});
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+window.addEventListener("load", reveal);
